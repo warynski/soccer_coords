83d8a775be4e371d82d1cf8159c4de5a825efcfc
w16 <pkoryz@gmail.com>
2017-07-26 18:05:09 +0200

    up

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 var playerPositions = data.player_positions;
+var steps = playerPositions.length;
 var team = new Array;
 var timer = false;
 var count = 0;
+var time;
 var button = document.getElementById('button');
+var panel = document.getElementById('panel');
 var pointer = document.getElementById('pointer');
-var steps = playerPositions.length;
-
-console.log(steps);
-
+var currentTime = document.getElementById('current-time');
+var totalTime = document.getElementById('total-time');
 var c = document.getElementById('canvas').getContext('2d');
-// c.fillStyle = '#f00';
-// c.fillRect(0,0,canvas.width,canvas.height);
 
 init();
 
-// init() initializes program and sets players positions objects array
+// init() initializes program, sets players positions objects array and game duration time
 
 function init() {   
 
@@ -35,20 +34,21 @@ function init() {
             }
         );
     }
-
-
-
+    totalTime.innerHTML = timeCount(parseInt(steps/10));
 }
+
+// this event fires up positions data update
 
 button.addEventListener('click',function() {
     if ( count == steps ) count = 0;
     dataService();
-    console.log(timer);
 })
 
 // dataService() updates players positions in 100 ms intervals
 
 function dataService() {
+
+    // getIndexOf catches proper object from array
 
     function getIndexOf(array,id) {
         var length = array.length;
@@ -59,6 +59,8 @@ function dataService() {
         }                      
         return false;
     }
+
+    // 'if' statement determines if 100ms update function runs or pauses
 
     if(timer){
       clearInterval(timer);
@@ -71,6 +73,8 @@ function dataService() {
                 team[getIndexOf(team,id)].posY = playerPositions[count][i][2];
             }
             count += 1;
+            time = parseInt(count/10);
+            currentTime.innerHTML = timeCount(time);
             if (count == (steps)) {
                 clearInterval(timer);
             }
@@ -78,14 +82,20 @@ function dataService() {
     }
 }
 
+// this function draws each updated object positions and drives progress bar
+
 function drawPlayers(){
 
     pointer.style.left = (1176/steps)*count+'px';
 
+    // v-blank
+
     c.beginPath();
-    c.fillStyle = 'rgba(51,255,119,0.5)';
+    c.fillStyle = 'rgba(51,255,119,0.25)';
     c.fillRect(0,0,canvas.width,canvas.height);
     c.closePath();
+
+    // field lines
 
     c.beginPath();
     c.moveTo(0,100);
@@ -95,6 +105,8 @@ function drawPlayers(){
     c.lineTo(canvas.width,canvas.height-100);
     c.stroke();
     c.closePath();
+
+        // players drawing magic here :) 
 
         for ( i = 0; i < team.length; i++) {
             if ( team[i].posX != 0 || team[i].posY != 0 ) {
@@ -114,6 +126,31 @@ function drawPlayers(){
 
 requestAnimationFrame(drawPlayers);
 
+// time flow control
 
+panel.addEventListener("mousedown", function(e){
+    panel.onmousemove = function(e) {
+        var cursorPosition = 0;
+        e.clientX < 1176 ? cursorPosition = e.clientX : cursorPosition = 1175;
+        count = parseInt((cursorPosition*steps)/1176);
+    }
+});
+panel.addEventListener("mouseup", function(e){
+    panel.onmousemove = null;
+    }
+);
 
+// time format function for current time counter and total time
 
+function timeCount(time) {   
+    var hrs = ~~(time / 3600);
+    var mins = ~~((time % 3600) / 60);
+    var secs = time % 60;
+    var ret = "";
+    if (hrs > 0) {
+        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+    }
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
+}
